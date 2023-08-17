92533480a0f8dedf63699bea35f549afd2186cf0
sjq1412 <sarahdev.cod3@gmail.com>
2023-08-17 22:46:11 +0800

    use env port if available

--- index.js
+++ index.js
@@ -95,6 +95,6 @@ app.post("/api/persons", (request, response) => {
     response.json(person)
 })
 
-const PORT = 3002;
+const PORT = process.env.port || 3002;
 app.listen(PORT) 
 console.log(`Server running on port ${PORT}`)
